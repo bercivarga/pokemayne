a76be8720e8c7ca037a30ee5c5c062ed2b873ba3
bercivarga <65171545+bercivarga@users.noreply.github.com>
2021-05-13 10:50:26 +0200

    Add keyboard control and close modal functionality

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { getJSON } from '../store';
 import { IndividualPokemonInterface } from '../interfaces';
 import PokemonModal from './PokemonModal';
@@ -9,9 +9,12 @@ export default function PokemonCard(props: { url: string }): JSX.Element {
 
 	const { url }: { url: string } = props;
 
-	const toggleModal = (): void => {
-		setShowModal(!showModal);
-	};
+	const toggleModal = useCallback(
+		(): void => {
+			setShowModal(!showModal);
+		},
+		[ showModal ]
+	);
 
 	useEffect(
 		(): void => {
@@ -25,33 +28,34 @@ export default function PokemonCard(props: { url: string }): JSX.Element {
 	);
 
 	return (
-		<button
-			type="button"
-			onClick={() => toggleModal()}
-			className="h-full w-full mr-4 mb-4 bg-gray-100 hover:bg-gray-200 inline-block p-4 rounded-lg cursor-pointer"
-		>
-			{showModal && <PokemonModal {...pokeData as any} />}
-			{/******************** fix any type ********************/}
-			<div className="flex flex-row items-center justify-center">
-				<div className="w-24 h-24">
-					<img
-						className="object-contain h-full w-full"
-						src={pokeData && pokeData.sprites.front_default}
-						alt={pokeData && pokeData.name}
-					/>
+		<div>
+			{showModal && <PokemonModal toggleModal={toggleModal} pokeData={pokeData as IndividualPokemonInterface} />}
+			<button
+				type="button"
+				onClick={() => toggleModal()}
+				className="h-full w-full mr-4 mb-4 bg-gray-100 hover:bg-gray-200 inline-block p-4 rounded-lg cursor-pointer"
+			>
+				<div className="flex flex-row items-center justify-center">
+					<div className="w-24 h-24">
+						<img
+							className="object-contain h-full w-full"
+							src={pokeData && pokeData.sprites.front_default}
+							alt={pokeData && pokeData.name}
+						/>
+					</div>
+					<div>
+						<div className="font-bold text-lg">
+							{pokeData ? (
+								pokeData.name.replace(pokeData.name[0], pokeData.name[0].toUpperCase())
+							) : (
+								<p className="h-6" />
+							)}
+						</div>
+						<p>Height: {pokeData && pokeData.height}</p>
+						<p>Base EXP: {pokeData && pokeData.base_experience}</p>
+					</div>
 				</div>
-				<div>
-					<p className="font-bold text-lg">
-						{pokeData ? (
-							pokeData.name.replace(pokeData.name[0], pokeData.name[0].toUpperCase())
-						) : (
-							<p className="h-6" />
-						)}
-					</p>
-					<p>Height: {pokeData && pokeData.height}</p>
-					<p>Base EXP: {pokeData && pokeData.base_experience}</p>
-				</div>
-			</div>
-		</button>
+			</button>
+		</div>
 	);
 }
--- src/components/PokemonModal.tsx
+++ src/components/PokemonModal.tsx
@@ -1,10 +1,38 @@
+import { useEffect } from 'react';
 import { IndividualPokemonInterface } from '../interfaces';
 
-export default function PokemonModal(pokeData: IndividualPokemonInterface): JSX.Element {
+type PropFunctionType = () => void;
+
+export default function PokemonModal({
+	toggleModal,
+	pokeData
+}: {
+	pokeData: IndividualPokemonInterface;
+	toggleModal: PropFunctionType;
+}): JSX.Element {
+	useEffect(
+		() => {
+			const close = (e: KeyboardEvent) => {
+				if (e.keyCode === 27) {
+					toggleModal();
+				}
+			};
+			window.addEventListener('keydown', close);
+			return () => window.removeEventListener('keydown', close);
+		},
+		[ toggleModal ]
+	);
+
 	if (!pokeData) return <h1>Something went wrong.</h1>;
 
 	return (
-		<div className="w-screen h-screen p-12 absolute inset-0 bg-gray-100 grid grid-cols-1 md:grid-cols-3 items-center">
+		<div className="w-screen h-screen p-12 fixed inset-0 bg-gray-100 grid grid-cols-1 md:grid-cols-3 items-center z-10">
+			<button
+				onClick={() => toggleModal()}
+				className="absolute top-12 right-12 rounded-full p-4 bg-gray-100 border-black border-2 leading-3"
+			>
+				X
+			</button>
 			<div className="flex flex-col justify-center items-center col-span-1">
 				<img
 					className="w-72"
